88aa49bcc0ffe44641b2816090fb68f3a261023f
KopylovaNA <74449680+KopylovaNA@users.noreply.github.com>
2020-11-14 12:04:05 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+
+</body>
+</html>
